4f5a5eaad7c54843b015d47af3ea79221833bcdf
Nathan <nmcaleese87@gmail.com>
2022-07-18 10:41:51 -0700

    preliminaries, base template, course class

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def courses_index(request):
+  return render(request, 'courses/index.html', { 'courses': courses })
